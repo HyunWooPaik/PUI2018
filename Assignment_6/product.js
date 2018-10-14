bf2ca274e19519cf2bf092eb5be2c1bcd06d4e8f
Hyun Woo Paik <paikhy91@gmail.com>
2018-10-13 22:48:09 -0400

    A6

--- Assignment_6/product.js
+++ Assignment_6/product.js
@@ -33,4 +33,53 @@ function loadPage() {
 		qntnums[3].classList.add("active");
 		finalPrice.textContent = "$48.00";
 	});
+
+
+	let glzsels = document.getElementsByClassName("glzsel");
+	let calorie = document.getElementById("calorie");
+	let sugar = document.getElementById("sugar");
+	let fat = document.getElementById("fat");
+
+	glzsels[0].addEventListener("click", function() {
+		for (let i = 0; i < 4; i++) {
+			glzsels[i].classList.remove("active");
+		}
+		glzsels[0].classList.add("active");
+	});
+
+	glzsels[1].addEventListener("click", function() {
+		for (let i = 0; i < 4; i++) {
+			glzsels[i].classList.remove("active");
+		}
+		glzsels[1].classList.add("active");
+		calorie.textContent = "290";
+		sugar.textContent = "29g";
+		fat.textContent = "5g";
+	});
+
+	glzsels[2].addEventListener("click", function() {
+		for (let i = 0; i < 4; i++) {
+			glzsels[i].classList.remove("active");
+		}
+		glzsels[2].classList.add("active");
+		glzsels[1].classList.add("active");
+		calorie.textContent = "330";
+		sugar.textContent = "27g";
+		fat.textContent = "5g";
+	});
+
+	glzsels[3].addEventListener("click", function() {
+		for (let i = 0; i < 4; i++) {
+			glzsels[i].classList.remove("active");
+		}
+		glzsels[3].classList.add("active");
+		glzsels[1].classList.add("active");
+		calorie.textContent = "420";
+		sugar.textContent = "36g";
+		fat.textContent = "7g";
+	});
+
+
+
+
 }
